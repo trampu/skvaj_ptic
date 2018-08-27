aee1d576389857d4691eb53e704c52db6477034d
trampu <trampu@gmail.com>
2018-08-27 11:23:09 +0300

    Shortly
    Delete obsolete code, more informational result

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #-*-coding:utf8;-*-
 #qpy:3
 #qpy:console
-print("This is console module")
-print("calculated day consume")
+print("This is console module calculated day consume")
 import random
 def rndm(firstA,i):
   c = firstA[2]/i
@@ -12,37 +11,29 @@ def rndm(firstA,i):
   c2 = int(c2)
   ai = random.randint(c2, c1)
   return ai
-i = 1
-ai = 0
+
 firstA = [1,1,1]
-firstA[0] = input("2000/10:before ")
-firstA[0] = int(firstA[0])
-firstA[1] = input("2000/10:after ")
-firstA[1] = int(firstA[1])
+firstA[0] = int (input("2000/10:before "))
+firstA[1] = int (input("2000/10:after "))
 firstA[2] = firstA[1] - firstA[0]
 
-b = 13
-day = input("day: ")
-wday = day = int(day)
-secondA = [1]
+d = day = int(input("day: "))
+secondA = secondB = secondD = [1]
 secondA = secondA*(day)
-secondB = [1]
 secondB = secondB*(day)
-secondD = [1]
 secondD = secondD*(day)
 second = [secondD, secondA, secondB]
 
-#print (second)
-#srok
-d = day
+print ("raznica", firstA[2])
 f = 0
-print ("nachalo", firstA[2], day)
+i = 1
+ai = 0
 while i <= day:
  if i == day:
   second[0][i-1] = i
   second[1][0] = firstA[2]
   second[2][i-1] = f + firstA[2] + firstA[0]
-  i = i +1
+  i = i + 1
  else:
   ai = rndm(firstA,d)
   second[0][d-1] = d
@@ -52,7 +43,6 @@ while i <= day:
   d = d - 1
   i = i + 1
   f = f + ai
-  print(d, ai, firstA[2])
-
-print ("finish", f,firstA[2])
-print ("all", second)
+print ("days", second[0])
+print ("potr", second[1])
+print ("itog", second[2])
